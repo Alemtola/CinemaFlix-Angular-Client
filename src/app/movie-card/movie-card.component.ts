aa85e130bd88ed506e848877cbd1b00adadf4a68
Alemayehu <88435881+Alemtola@users.noreply.github.com>
2022-01-26 17:55:27 +0100

    updated movie card elements & styling

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 import { GenreComponent } from '../genre/genre.component';
 import { DirectorComponent } from '../director/director.component';
 import { DescriptionComponent } from '../description/description.component';
+import { FavouritesComponent } from '../favourites/favourites.component';
 
 
 @Component({
@@ -13,8 +16,12 @@ import { DescriptionComponent } from '../description/description.component';
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
-
+  
+  username = localStorage.getItem('user') || '';
+  user: any = JSON.parse(localStorage.getItem('user') || '');
   movies: any[] = [];
+  favourites: any[] = [];
+
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog, 
@@ -23,6 +30,8 @@ export class MovieCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getMovies();
+    this.getFavouriteMovies();
+    this.isFav;
   }
 
   getMovies(): void {
@@ -53,4 +62,68 @@ export class MovieCardComponent implements OnInit {
         width: '300px' 
       });
     }
+
+    getFavouriteMovies(): void {
+      this.fetchApiData.getUser(this.username!).subscribe((resp: any) => { 
+        this.favourites = resp.FavouriteMovies 
+      });
+    }
+
+    getUserFavs(): any {
+      this.fetchApiData.getFavoriteMovies(this.user.Username).subscribe((res: any) => {
+        this.favourites = res.Favorites;
+        return this.favourites;
+      });
+    }
+  
+    deleteFavouriteMovie(movieID: string, title: string): void {
+      this.fetchApiData.deleteMovie(movieID).subscribe((resp: any) => { 
+        this.favourites = resp;
+        this.snackBar.open(`${title} has been removed from your favourites!`, 'Ok', { duration: 4000, panelClass: 'snack-style' });
+       }, (result) => {
+         console.log(result);
+         this.snackBar.open(`We couldn't unfavourite ${title}. Please try again`, 'Ok', {
+           duration: 4000
+         }); 
+      });
+    }
+
+    openFavouritesDialog(): void {
+      this.dialog.open(FavouritesComponent, {
+        width: '100%',
+      })
+    }
+
+    addToFavs(movieId: string, title: string): void {
+      this.fetchApiData
+        .addMovie(this.user.Username)
+        .subscribe((res: any) => {
+          this.snackBar.open(
+            `${title} has been added to your favorite movies! ✔️`,
+            'Cool',
+            {
+              duration: 2000,
+            }
+          );
+          this.ngOnInit();
+        });
+      return this.getUserFavs();
+    }
+  
+    isFav(movieId: string): boolean {
+      return this.favourites.some((movie) => movie._id === movieId);
+    }
+  
+    /**
+     * Toggles the heart shaped icon from full to empty, and invokes the method to add or
+     * remove a function from the user's list of favorites
+     * @function toggleFavs
+     * @param movie the movie to add/remove to/from favs
+     */
+    toggleFavs(movie: any): void {
+      this.isFav(movie._id)
+        ? this.deleteFavouriteMovie(movie._id, movie.Title)
+        : this.addToFavs(movie._id, movie.Title);
+    }
+  
 }
